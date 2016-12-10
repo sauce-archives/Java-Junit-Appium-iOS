e473dc4671ace4b12b43bab5b653fb09c07bd6ed
Titus Fortner <titusfortner@gmail.com>
2016-12-10 15:44:25 -0600

    Page Objects implemented

--- src/test/java/com/yourcompany/Pages/GuineaPigPage.java
+++ src/test/java/com/yourcompany/Pages/GuineaPigPage.java
@@ -1,5 +1,6 @@
 package com.yourcompany.Pages;
 
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -7,33 +8,17 @@ import org.openqa.selenium.support.PageFactory;
 
 import io.appium.java_client.MobileElement;
 
-/**
- * Created by mehmetgerceker on 12/21/15.
- */
-
-public class GuineaPigPage extends PageBase {
-
+public class GuineaPigPage {
 
     @FindBy(id = "h1Text")
     private WebElement h1Text;
 
-    @FindBy(id = "unchecked_checkbox")
-    private WebElement uncheckedCheckbox;
-
-    @FindBy(id = "checked_checkbox")
-    private WebElement checkedCheckbox;
-
-    @FindBy(id = "i_am_a_link")
+    @FindBy(id = "i am a link")
     private WebElement theActiveLink;
 
-    @FindBy(id = "i_am_a_textbox")
-    private WebElement textInput;
-
-    @FindBy(id = "your_comments")
-    private WebElement yourCommentsSpan;
-
-    @FindBy(id = "fbemail")
-    private WebElement emailTextInput;
+    //  @FindBy(xpath = "//*[@id='Your comments’]/following-sibling::*")
+    @FindBy(xpath = "//XCUIElementTypeApplication[1]/XCUIElementTypeWindow[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeStaticText[6]")
+    private WebElement yourComments;
 
     @FindBy(id = "comments")
     private WebElement commentsTextInput;
@@ -41,66 +26,42 @@ public class GuineaPigPage extends PageBase {
     @FindBy(id = "submit")
     private WebElement submitButton;
 
-    public static GuineaPigPage getPage(WebDriver driver) {
-        return PageFactory.initElements(driver, GuineaPigPage.class);
+    public WebDriver driver;
+
+    public GuineaPigPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
-    public void checkUncheckedSwitch() {
-        setCheckSwitchState(this.uncheckedCheckbox, true);
-    }
-
-    public boolean getUncheckedSwitchState() {
-        return getSwitchState(this.checkedCheckbox);
-    }
-
-    public void uncheckCheckedSwitch() {
-        setCheckSwitchState(this.checkedCheckbox, false);
-    }
-
-    public boolean getCheckedSwitchState() {
-        return getSwitchState(this.checkedCheckbox);
-    }
-
-    public void enterCommentText(String text) {
+    public void submitComment(String text) {
         this.commentsTextInput.click();
-        setTextInputValue(this.commentsTextInput, text);
+        this.commentsTextInput.sendKeys(text);
+        hideKeyboard();
+        this.submitButton.click();
     }
-    public void clickCommentText() {
-        this.commentsTextInput.click();
-    }
-    public void clearCommentText() {
-        this.commentsTextInput.clear();
-    }
-    public String getCommentText() {
-        return this.commentsTextInput.getAttribute("value");
-    }
-    public void submitForm() {
-        clickButton(this.submitButton);
-    }
+
     public String getSubmittedCommentText() {
-        return this.yourCommentsSpan.getText();
+        return this.yourComments.getText();
     }
 
-    public void enterEmailText(String email) {
-        setTextInputValue(this.emailTextInput, email);
+    public void followLink() {
+        this.theActiveLink.click();
     }
-    public void clickEmailText() {
-        this.emailTextInput.click();
-    }
-    public void clearEmailText() {
-        this.emailTextInput.clear();
-    }
-    public String getEmailText() {
-        return this.emailTextInput.getText();
+
+    public boolean isOnPage() {
+        try {
+            getSubmittedCommentText();
+            return true;
+        } catch (NoSuchElementException ex) {
+            return false;
+        }
     }
 
     /**
      * This method only work for this page and assumes the app supports keyboard hide on click-away.
      * In appium there's no way of doing this with a generalized method for iOS as of yet.
      */
-    public void hideKeyboard(){
+    public void hideKeyboard() {
         this.h1Text.click();
     }
-
 }
-
