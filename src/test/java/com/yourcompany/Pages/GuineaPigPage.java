e136c38b71ab9b1b115d9620eeb3add337fb7580
ndmanvar <ndmanvar@yahoo.com>
2016-12-11 20:30:12 -0800

    using 7 instead of 6 in xpath selector

--- src/test/java/com/yourcompany/Pages/GuineaPigPage.java
+++ src/test/java/com/yourcompany/Pages/GuineaPigPage.java
@@ -17,7 +17,7 @@ public class GuineaPigPage {
     private WebElement theActiveLink;
 
     //  @FindBy(xpath = "//*[@id='Your comments’]/following-sibling::*")
-    @FindBy(xpath = "//XCUIElementTypeApplication[1]/XCUIElementTypeWindow[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeStaticText[6]")
+    @FindBy(xpath = "//XCUIElementTypeApplication[1]/XCUIElementTypeWindow[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeStaticText[7]")
     private WebElement yourComments;
 
     @FindBy(id = "comments")
